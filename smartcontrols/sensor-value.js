c3ff7302b582c310221238fbb6c75c374ae3f4fc
davidhkates <davidhkates@yahoo.com>
2021-05-19 09:50:27 -0700

    Fixed contact sensor, added logic for device map

--- smartcontrols/sensor-value.js
+++ smartcontrols/sensor-value.js
@@ -2,20 +2,24 @@
    Convenience functions to retrieve state/value of various sensors
 */
 
-// get the temperature value of specified sensor
+// get the temperature value of specified temperature measurement sensor
 async function getTemperature( context, sensor ) {
 	try {
-		const sensorState = await context.api.devices.getCapabilityStatus( sensor.deviceConfig.deviceId, sensor.deviceConfig.componentId, 'temperatureMeasurement');
-		return sensorState[0].temperature.value;
+		const sensorMap = context.config.sensor;
+		if ( sensorMap.length == 1 ) {
+			const sensorDevice = sensorMap[0].deviceConfig;
+			const sensorState = await context.api.devices.getCapabilityStatus( sensorDevice.deviceId, sensorDevice.componentId, 'temperatureMeasurement');
+			return sensorState[0].temperature.value;
+		}
 	} catch (err) {
 		console.log("Error", err);
 	}	
 };	
 
-// get the contact state of specified sensor
+// get the contact state of specified contact sensor
 async function getContact( context, sensor ) {
 	try {
-		const sensorState = await context.api.devices.getCapabilityStatus( sensor.deviceConfig.deviceId, sensor.deviceConfig.componentId, 'contactState');
+		const sensorState = await context.api.devices.getCapabilityStatus( sensor.deviceConfig.deviceId, sensor.deviceConfig.componentId, 'contactSensor');
 		return sensorState[0].contact.value;
 	} catch (err) {
 		console.log("Error", err);
