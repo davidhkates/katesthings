4b4bd0deb067db4b515f538fbc90d6d58b2a7eab
davidhkates <davidhkates@yahoo.com>
2021-05-22 13:22:48 -0700

    Update time-functions.js

--- smartutils/time-functions.js
+++ smartutils/time-functions.js
@@ -4,21 +4,23 @@ function inTimeWindow( startDateTime, endDateTime ) {
 	// initialize return value
 	var inTimeWindow = true;
 	
-	if ( startDateTime ) {
+	if (startDateTime != endDateTime) {
 		// apply current date to start and end date/time
 		const currentDate = new Date();
 		startDateTime.setFullYear( currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() );
-		inTimeWindow = ( currentDate >= startDateTime );
-		
-		if ( endDateTime && inTimeWindow ) {
-			endDateTime.setFullYear( currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() );
+		endDateTime.setFullYear( currentDate.getFullYear(), currentDate.getMonth(), currentDate.getDate() );
 
-			// if times cross midnight, add a day to end date/time
-			if ( startDateTime > endDateTime ) {
-				endDateTime.setDate(endDateTime.getDate() + 1);
+		// check to see if span midnight, if so, adjust basec on current time
+		if ( startDateTime > endDateTime ) {
+			if (currentDate > startDateTime) {
+				endDateTime.setDate(endDateTime.getDate()+1);
+			} else {
+				startDateTime.setDate(startDateTime.getDate()-1)
 			}
-			inTimeWindow = ( currentDate <= endDateTime );
 		}
+				
+		// check to see if current time is between start and end time		
+		inTimeWindow = ( (currentDate >= startDateTime) && (currentDate <= endDateTime) );
 	}
 	return inTimeWindow;
 }
